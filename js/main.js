ea707f3123fe61b385b43836762890ea38e2109a
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-06-27 15:50:44 -0400

    Update main.js
    battle

--- js/main.js
+++ js/main.js
@@ -14,15 +14,21 @@ function draw() {
     switch (gameState) {
         case OVERWORLD:
             drawOverworld();
+            // hides the gif
+           document.getElementById("mypokemon").style.visibility = "hidden";
+           document.getElementById("encounterpokemon").style.visibility = "hidden";
+
             break;
 
         case BATTLE:
             drawBattle();
+
             break;
     }
 }
 
 function gameLoop() {
+
     update();
     draw();
     requestAnimationFrame(gameLoop);
