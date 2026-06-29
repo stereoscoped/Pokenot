965e9b41cf91c4156274f3d3da7378a3223e8d41
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-06-29 10:44:50 -0400

    Update collisions.js

--- js/collisions.js
+++ js/collisions.js
@@ -1,8 +1,15 @@
-//collision check bt
-// wn objects
 let grassParticles = [];
 const MAX_GRASS_PARTICLES = 100;
 
+// let playerBox =
+// {
+//     x: player.x,
+//     y: player.y + player.height / 2,
+//     w: player.width,
+//     h: player.height / 2
+// };
+
+//collision check btwn objects
 function collides(a, b) {
     return (
         a.x < b.x + b.w &&
@@ -29,21 +36,10 @@ function collideMapChange() {
 
 //grass encounter
 function collideGrass() {
-    let playerBox =
-        {
-            x: player.x,
-            y: player.y,
-            w: player.width,
-            h: player.height
-        };
-
-    for (let patch of grass) {
+    for (let patch of currentMap.grass) {
         if (collides(playerBox, patch)) {
             if (Math.random() < 0.2) {
                 spawnGrassParticles();
-            }
-            if (Math.random() < 0.005) {
-                battleTransition();
             }
             if (Math.random() < 0.005) {
                 battleTransition();
@@ -120,83 +116,11 @@ function updateGrassParticles() {
             grassParticles.splice(i, 1);
         }
     }
-
-
-}
-
-function spawnGrassParticles() {
-
-    if (grassParticles.length > MAX_GRASS_PARTICLES) {
-        return;
-    }
-
-    for (let i = 0; i < 4; i++) {
-
-        let particle =
-            {
-                x: player.x + player.width / 2,
-                y: player.y + player.height,
-
-                vx: (Math.random() - 0.5) * 1.5,
-                vy: -1 - Math.random(),
-
-                life: 20,
-                size: 4 + Math.random() * 3,
-
-                //rand rotation
-                angle: (Math.random() - 0.5) * 0.8,       // Initial angle (≈ ±45°)
-                rotationSpeed: (Math.random() - 0.5) * 0.15
-            };
-
-        //throw grass depending on player movement?
-        switch (player.direction) {
-
-            // case "up":
-            //     particle.vy = 1 + Math.random();
-            //     break;
-
-            // case "down":
-            //     particle.vy = -1 - Math.random();
-            //     break;
-
-            // case "left":
-            //     particle.vx = 1 + Math.random();
-            //     break;
-
-            // case "right":
-            //     particle.vx = -1 - Math.random();
-            //     break;
-        }
-
-        grassParticles.push(particle);
-    }
-}
-
-function updateGrassParticles() {
-
-    for (let i = grassParticles.length - 1; i >= 0; i--) {
-
-        let p = grassParticles[i];
-
-        p.x += p.vx;
-        p.y += p.vy;
-
-        //bit of gravity
-        p.vy += 0.15;
-
-        p.angle += p.rotationSpeed;
-
-        p.life--;
-
-        if (p.life <= 0) {
-            grassParticles.splice(i, 1);
-        }
-    }
 }
 
 function drawGrassParticles() {
 
-    ctx.strokeStyle = "#3f8f3f";
+    ctx.strokeStyle = "#35a535";
     ctx.lineWidth = 2;
 
     for (let p of grassParticles) {
