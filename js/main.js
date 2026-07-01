ef86305b5cd02f5dd07404eeaf033cda14bdf105
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-07-01 01:19:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,28 +14,46 @@ function update() {
         case BATTLE:
             updateBattle();
             break;
+        case STARTER:
+            update_starter();
+            break;
+
     }
 }
 
 function draw() {
     switch (gameState) {
         case MENU:
+            document.getElementById("btn_stater_1").style.visibility = "hidden";
+            document.getElementById("btn_stater_2").style.visibility = "hidden";
+            document.getElementById("btn_stater_3").style.visibility = "hidden";
             start_Game();
             break;
         case OVERWORLD:
             drawOverworld();
             document.getElementById("mypokemon").style.visibility = "hidden";
             document.getElementById("encounterpokemon").style.visibility = "hidden";
+            document.getElementById("btn_stater_1").style.visibility = "hidden";
+            document.getElementById("btn_stater_2").style.visibility = "hidden";
+            document.getElementById("btn_stater_3").style.visibility = "hidden";
+
 
             break;
 
         case BATTLE_START:
+
             drawTransition();
             break;
 
         case BATTLE:
+
             drawBattle();
             break;
+        case STARTER:
+            drawStater();
+            document.getElementById("btn_stater_1").style.visibility = "visible";
+            document.getElementById("btn_stater_2").style.visibility = "visible";
+            document.getElementById("btn_stater_3").style.visibility = "visible";
     }
 }
 
@@ -47,5 +65,6 @@ function gameLoop() {
 
 
 trainer.onload = function () {
+    loadPokemonDatabase();
     gameLoop();
 };
