97b6b63259f559d1b81d636ed5a03e8531a2a561
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-07-01 01:20:08 -0400

    Update overworld.js

--- js/overworld.js
+++ js/overworld.js
@@ -1,140 +1,320 @@
-const MAP_SCALE = 2.5;
+//playerdata
+const PLAYER_SCALE = 0.75; // additional scale applied to player sprite and hitbox
 
-let camera = {
-    x: 0,
-    y: 0
+let player = {
+    x: 150,
+    y: 90,
+
+    width: 25,
+    height: 30,
+
+    direction: "down",
+
+    frame: 0,
+    frameCounter: 0,
+    framesUntilUpdate: 8,
+
+    speed: 1.5,
+    jumpOffset: 0,
 };
 
-function loadMap(mapName, entrance) {
-    currentMap = maps[mapName];
+let playerBox = [];
 
-    player.x = currentMap.spawnPoints[entrance].x;
-    player.y = currentMap.spawnPoints[entrance].y;
-    player.direction = currentMap.spawnPoints[entrance].direction;
+const animations = {
+    down:
+    {
+        startX: 280,
+        startY: 50,
+        frameWidth: 23,
+        frameTot: 4
+    },
 
-    camera.x = 0;
-    camera.y = 0;
+    up:
+    {
+        startX: 280,
+        startY: 154,
+        frameWidth: 23,
+        frameTot: 4
+    },
 
-    playMusic(currentMap.music);
+    right:
+    {
+        startX: 275,
+        startY: 85,
+        frameWidth: 25,
+        frameTot: 4
+    },
+
+    left:
+    {
+        startX: 275,
+        startY: 118,
+        frameWidth: 25,
+        frameTot: 4
+    }
+};
+
+let jumpTimer = 0;
+const JUMP_TIME = 18;
+
+let jumping = false;
+let jumpStartY = 0;
+let jumpEndY = 0;
+
+let downHeld = 0;
+const DOWN_HOLD_TIME = 18;
+
+function getPlayerBox(x = player.x, y = player.y) {
+    return {
+        x: x + player.width * PLAYER_SCALE * 0.2,
+        y: y + player.height * PLAYER_SCALE * 0.75,
+        w: player.width * PLAYER_SCALE * 0.6,
+        h: player.height * PLAYER_SCALE / 4
+    };
 }
 
-function updateOverworld() {
-    updatePlayer();
-    updateCamera();
-    updateGrassParticles();
+function startJump(ledge) {
+    jumping = true;
+    jumpTimer = 0;
+    jumpStartY = player.y;
+    jumpEndY = ledge.y + 16;
 }
 
-function updateCamera() {
+function updateJump() {
+    jumpTimer++;
+    let t = jumpTimer / JUMP_TIME;
 
-    // Keep player centered
-    camera.x =
+    if (t > 1)
+        t = 1;
+
+    player.y =
+        jumpStartY +
+        (jumpEndY - jumpStartY) * t;
+
+
+    player.jumpOffset =
+        -Math.sin(t * Math.PI) * 12;
+
+    if (t >= 1) {
+        jumping = false;
+        player.jumpOffset = 0;
+    }
+}
+
+function movePlayerX(dx) {
+    if (dx === 0)
+        return;
+
+    let newX = player.x + dx;
+
+    let box = getPlayerBox(newX, player.y);
+
+    let blocked = false;
+
+    for (let wall of currentMap.walls) {
+        if (collides(box, wall)) {
+            blocked = true;
+            break;
+        }
+    }
+
+    if (!blocked) {
+        player.x = newX;
+    }
+}
+
+function movePlayerY(dy) {
+    if (dy === 0)
+        return;
+
+    let newY = player.y + dy;
+
+    let box = getPlayerBox(player.x, newY);
+
+    let blocked = false;
+
+    for (let wall of currentMap.walls) {
+        if (collides(box, wall)) {
+            blocked = true;
+            break;
+        }
+    }
+
+    if (!blocked) {
+        playerBox = box;
+        blocked = collideLedges();
+    }
+
+    if (!blocked) {
+        player.y = newY;
+    }
+}
+
+function updatePlayer() {
+    if (jumping) {
+        updateJump();
+        return;
+    }
+
+    let dx = 0;
+    let dy = 0;
+
+    if (keys["d"] || keys["arrowright"])
+        dx++;
+
+    if (keys["a"] || keys["arrowleft"])
+        dx--;
+
+    if (keys["w"] || keys["arrowup"])
+        dy--;
+
+    if (keys["s"] || keys["arrowdown"])
+        dy++;
+
+    //update look dir -> prioritize horizontal
+    if (dx > 0)
+        player.direction = "right";
+    else if (dx < 0)
+        player.direction = "left";
+    else if (dy > 0)
+        player.direction = "down";
+    else if (dy < 0)
+        player.direction = "up";
+
+    //normalize diagonal speed
+    let moving = (dx !== 0 || dy !== 0);
+
+    if (moving) {
+        let length = Math.sqrt(dx * dx + dy * dy);
+
+        dx /= length;
+        dy /= length;
+
+        dx *= player.speed;
+        dy *= player.speed;
+    }
+
+    movePlayerX(dx);
+    movePlayerY(dy);
+
+    player.x = Math.max(
+        0,
+        Math.min(
+            player.x,
+            currentMap.width - player.width * PLAYER_SCALE
+        )
+    );
+
+    player.y = Math.max(
+        0,
+        Math.min(
+            player.y,
+            currentMap.height - player.height * PLAYER_SCALE
+        )
+    );
+
+    //update hitbox
+    playerBox = getPlayerBox();
+
+    //collision checks that dont affect movement
+    if (moving) {
+        collideGrass();
+        collideMapChange();
+    }
+
+    if (moving) {
+        player.frameCounter++;
+
+        if (player.frameCounter > player.framesUntilUpdate) {
+            player.frame++;
+            player.frame %= animations[player.direction].frameTot;
+            player.frameCounter = 0;
+        }
+    }
+    else {
+        player.frame = 1;
+        player.frameCounter = 0;
+    }
+}
+
+function drawPlayer() {
+    //select anim set based on direction
+    let anim =
+        animations[player.direction];
+
+    //get coords of current frame in sprite sheet
+    let sourceX =
+        anim.startX +
+        player.frame * anim.frameWidth;
+
+    let sourceY =
+        anim.startY;
+
+    let drawX =
         player.x * MAP_SCALE -
-        canvas.width / 2;
+        camera.x;
 
-    camera.y =
+    let drawY =
         player.y * MAP_SCALE -
-        canvas.height / 2;
+        camera.y +
+        player.jumpOffset;
 
-    // Prevent seeing past the left/top edges
-    camera.x = Math.max(0, camera.x);
-    camera.y = Math.max(0, camera.y);
+    ctx.fillStyle = "rgba(0,0,0,0.25)";
 
-    // Prevent seeing past the right/bottom edges
-    camera.x = Math.min(
-        camera.x,
-        currentMap.width * MAP_SCALE - canvas.width
+    ctx.beginPath();
+
+    ctx.ellipse(
+        drawX + player.width * MAP_SCALE * PLAYER_SCALE / 2,
+        player.y * MAP_SCALE - camera.y + player.height * MAP_SCALE * PLAYER_SCALE,
+
+        10,
+        4,
+        0,
+        0,
+        Math.PI * 2
     );
 
-    camera.y = Math.min(
-        camera.y,
-        currentMap.height * MAP_SCALE - canvas.height
+    ctx.fill();
+    ctx.drawImage(
+        trainer,
+
+        sourceX,
+        sourceY,
+
+        anim.frameWidth,
+        player.height,
+
+        drawX,
+        drawY,
+
+        player.width * MAP_SCALE * PLAYER_SCALE,
+        player.height * MAP_SCALE * PLAYER_SCALE
     );
 }
 
-function drawOverworld() {
-    ctx.clearRect(
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
+function drawPlayerTransition() {
 
-    // //town image
-    // ctx.drawImage(
-    //     currentMap.image,
-    //     0,
-    //     0,
-    //     canvas.width,
-    //     canvas.height
-    // );
+    let anim = animations[player.direction];
+
+    let sourceX =
+        anim.startX +
+        player.frame * anim.frameWidth;
+
+    let sourceY = anim.startY;
 
     ctx.drawImage(
-        currentMap.image,
+        trainer,
 
-        -camera.x,
-        -camera.y,
+        sourceX,
+        sourceY,
 
-        currentMap.width * MAP_SCALE,
-        currentMap.height * MAP_SCALE
+        anim.frameWidth,
+        player.height,
+
+        player.x * MAP_SCALE,
+        player.y * MAP_SCALE,
+
+        player.width * MAP_SCALE * PLAYER_SCALE,
+        player.height * MAP_SCALE * PLAYER_SCALE
     );
-
-    //debug collision boxes
-    for (let wall of currentMap.walls) {
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(
-            wall.x * MAP_SCALE - camera.x,
-            wall.y * MAP_SCALE - camera.y,
-
-            wall.w * MAP_SCALE,
-            wall.h * MAP_SCALE
-        );
-    }
-
-   //debug collision boxes
-    for (let ledge of currentMap.ledges) {
-        ctx.strokeStyle = "white";
-        ctx.strokeRect(
-            ledge.x * MAP_SCALE - camera.x,
-            ledge.y * MAP_SCALE - camera.y,
-
-            ledge.w * MAP_SCALE,
-            ledge.h * MAP_SCALE
-        );
-    }
-
-    //debug grass
-    for (let gras of currentMap.grass) {
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(
-            gras.x * MAP_SCALE - camera.x,
-            gras.y * MAP_SCALE - camera.y,
-            gras.w * MAP_SCALE,
-            gras.h * MAP_SCALE
-        );
-    }
-
-    //debug exit
-    for (let exit of currentMap.exits) {
-        ctx.strokeStyle = "blue";
-        ctx.strokeRect(
-            exit.x * MAP_SCALE - camera.x,
-            exit.y * MAP_SCALE - camera.y,
-            exit.w * MAP_SCALE,
-            exit.h * MAP_SCALE
-        );
-    }
-
-    //debug player collision box
-    if (playerBox && typeof playerBox.x !== 'undefined') {
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(
-            playerBox.x * MAP_SCALE - camera.x,
-            playerBox.y * MAP_SCALE - camera.y,
-            playerBox.w * MAP_SCALE,
-            playerBox.h * MAP_SCALE
-        );
-    }
-
-    drawPlayer();
-    drawGrassParticles();
 }
