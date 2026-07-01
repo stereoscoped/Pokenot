976e711b37e085ee2519a51e117352257aa2f038
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-07-01 01:19:47 -0400

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,47 @@
+const labMap = {
+    name: "lab",
+
+    image: lab,
+
+    music: "labBGM",
+
+    width: 320,
+    height: 277,
+    walls: [],
+
+    ledges: [],
+
+    grass: [],
+
+    exits: [
+        {
+            x: 137,
+            y: 275,
+            w: 50,
+            h: 30,
+
+            destination: "town",
+            entrance: "lab"
+        },
+    ],
+
+    spawnPoints: {
+        door:
+        {
+            x: 150,
+            y: 250,
+
+            direction: "up"
+        }
+    }
+};
+
 const townMap = {
     name: "Town",
 
     image: town,
 
-    music: "town",
+    music: "townBGM",
 
     width: 320,
     height: 280,
@@ -51,10 +89,10 @@ const townMap = {
         // =========================
 
         // sign beside left house
-        { x: 50, y: 78, w: 13, h: 14 },
+        { x: 50, y: 78, w: 13, h: 12 },
 
         // sign beside right house
-        { x: 178, y: 78, w: 13, h: 14 },
+        { x: 179, y: 78, w: 13, h: 12 },
 
 
         // =========================
@@ -76,9 +114,9 @@ const townMap = {
         { x: 63, y: 210, w: 67, h: 70 },
     ],
 
-    grass: [
-        // { x: 400, y: 0, w: 80, h: 67 }
-    ],
+    ledges: [],
+
+    grass: [],
 
     exits: [
         {
@@ -89,6 +127,15 @@ const townMap = {
 
             destination: "route1",
             entrance: "south"
+        },
+        {
+            x: 197,
+            y: 175,
+            w: 13,
+            h: 10,
+
+            destination: "lab",
+            entrance: "door"
         }
     ],
 
@@ -97,6 +144,13 @@ const townMap = {
         {
             x: 175,
             y: 30,
+
+            direction: "down"
+        },
+        lab:
+        {
+            x: 195,
+            y: 175,
 
             direction: "down"
         }
@@ -109,7 +163,7 @@ const route1Map = {
 
     image: route,
 
-    music: "route",
+    music: "routeBGM",
 
     width: 320,
     height: 576,
@@ -129,6 +183,39 @@ const route1Map = {
         { x: 198, y: 22, w: 78, h: 12 },
 
 
+        // =========================
+        // TREE / BUSH HITBOXES
+        // small body/trunk area only
+        // =========================
+
+        // vertical tree line beside top grass
+        // ends before x = 160 so it does NOT block your grass patch
+        { x: 148, y: 65, w: 12, h: 92 },
+
+        // left pair of bushes/trees
+        { x: 75, y: 205, w: 28, h: 18 },
+
+        // center pair of bushes/trees
+        { x: 162, y: 205, w: 30, h: 18 },
+
+        // lower horizontal tree row
+        // ends before x = 192 so it does NOT block your lower-right grass
+        { x: 76, y: 365, w: 114, h: 10 },
+
+
+        // =========================
+        // BOTTOM STONE ROWS
+        // leave the middle exit open
+        // =========================
+        { x: 75, y: 544, w: 82, h: 12 },
+        { x: 198, y: 544, w: 78, h: 12 },
+
+        // stones on sides of bottom exit
+        { x: 146, y: 545, w: 13, h: 32 },
+        { x: 195, y: 545, w: 13, h: 32 },
+    ],
+
+    ledges: [
         // =========================
         // LOG FENCES
         // only block the logs, not the grass/path
@@ -157,42 +244,10 @@ const route1Map = {
 
         // lower-right log, placed above grass so it does not block encounter grass
         { x: 206, y: 442, w: 74, h: 8 },
-
-
-        // =========================
-        // TREE / BUSH HITBOXES
-        // small body/trunk area only
-        // =========================
-
-        // vertical tree line beside top grass
-        // ends before x = 160 so it does NOT block your grass patch
-        { x: 148, y: 65, w: 12, h: 92 },
-
-        // left pair of bushes/trees
-        { x: 78, y: 200, w: 28, h: 22 },
-
-        // center pair of bushes/trees
-        { x: 162, y: 200, w: 30, h: 20 },
-
-        // lower horizontal tree row
-        // ends before x = 192 so it does NOT block your lower-right grass
-        { x: 76, y: 365, w: 114, h: 10 },
-
-
-        // =========================
-        // BOTTOM STONE ROWS
-        // leave the middle exit open
-        // =========================
-        { x: 75, y: 544, w: 82, h: 12 },
-        { x: 198, y: 544, w: 78, h: 12 },
-
-        // stones on sides of bottom exit
-        { x: 146, y: 545, w: 13, h: 32 },
-        { x: 195, y: 545, w: 13, h: 32 },
     ],
 
     grass: [
-         { x: 160, y: 95, w: 200, h: 65 },
+        { x: 160, y: 95, w: 200, h: 65 },
         { x: 225, y: 190, w: 200, h: 65 },
         { x: 192, y: 350, w: 63, h: 65 },
         { x: 64, y: 450, w: 250, h: 250 },
@@ -207,6 +262,15 @@ const route1Map = {
 
             destination: "town",
             entrance: "north"
+        },
+        {
+            x: 155,
+            y: 0,
+            w: 40,
+            h: 20,
+
+            destination: "caveEntr",
+            entrance: "south"
         }
     ],
 
@@ -217,6 +281,137 @@ const route1Map = {
             y: 520,
 
             direction: "up"
+        },
+        north:
+        {
+            x: 175,
+            y: 30,
+
+            direction: "down"
+        }
+    }
+};
+
+const caveEntrMap = {
+    name: "caveEntr",
+
+    image: cave_entr,
+
+    music: "caveEntrBGM",
+
+    width: 320,
+    height: 568,
+    walls: [
+        // left wall
+        { x: 0, y: 0, w: 136, h: 440 },
+        { x: 0, y: 0, w: 70, h: 4400 },
+        { x: 0, y: 545, w: 95, h: 400 },
+
+
+        // // right wall
+        { x: 182, y: 0, w: 136, h: 440 },
+        { x: 250, y: 0, w: 70, h: 4400 },
+        { x: 225, y: 545, w: 70, h: 4400 },
+
+    ],
+
+    ledges: [],
+
+    grass: [],
+
+    exits: [
+        {
+            x: 95,
+            y: 560,
+            w: 130,
+            h: 30,
+
+            destination: "route1",
+            entrance: "north"
+        },
+        {
+            x: 140,
+            y: 70,
+            w: 40,
+            h: 10,
+
+            destination: "cave",
+            entrance: "south"
+        }
+    ],
+
+    spawnPoints: {
+        south:
+        {
+            x: 150,
+            y: 530,
+
+            direction: "up"
+        },
+        north:
+        {
+            x: 150,
+            y: 80,
+
+            direction: "down"
+        }
+    }
+};
+
+const caveMap = {
+    name: "cave",
+
+    image: cave,
+
+    music: "caveBGM",
+
+    width: 320,
+    height: 480,
+    walls: [
+        { x: 0, y: 0, w: 1000, h: 166 },
+        { x: 0, y: 420, w: 1000, h: 166 },
+    
+
+        { x: 0, y: 0, w: 90, h: 1000 },
+        { x: 0, y: 275, w: 145, h: 15 },
+        { x: 0, y: 280, w: 130, h: 4400 },
+
+        { x: 230, y: 0, w: 90, h: 1000 },
+        { x: 175, y: 275, w: 145, h: 15 },
+        { x: 190, y: 280, w: 130, h: 4400 },
+
+    ],
+
+    ledges: [],
+
+    grass: [],
+
+    exits: [
+        {
+            x: 144,
+            y: 415,
+            w: 30,
+            h: 30,
+
+            destination: "caveEntr",
+            entrance: "north"
+        },
+    ],
+
+    spawnPoints: {
+        south:
+        {
+            x: 150,
+            y: 390,
+
+            direction: "up"
+        },
+        north:
+        {
+            x: 175,
+            y: 30,
+
+            direction: "down"
         }
     }
 };
@@ -225,7 +420,9 @@ const route1Map = {
 const maps = {
     town: townMap,
     route1: route1Map,
-    // gym: gymMap
+    caveEntr: caveEntrMap,
+    cave: caveMap,
+    lab: labMap
 };
 
-let currentMap = townMap;
+let currentMap = labMap;
