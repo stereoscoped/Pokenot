af30ec911cd65cad083cc57813ca554ce3c4e187
jbanb21 <joebanbury21@gmail.com>
2026-07-02 01:47:16 -0400

    Pokemon & their attacks now fetched from database

--- js/pokemon.js
+++ js/pokemon.js
@@ -9,9 +9,8 @@ class Pokemon {
         this.attack = parseInt(data.attack);
         this.defense = parseInt(data.defense);
         this.speed = parseInt(data.speed);
-        this.imageUrl = data.image_url;
-        
-        this.attacks = data.moves || [];
+        this.image_url = data.image_url; 
+        this.moves = data.moves || []; 
     }
 }
 
@@ -36,12 +35,14 @@ async function loadPokemonDatabase() {
         
         // Transform raw database rows into instance objects
         pokeDex = rawData.map(item => new Pokemon(item));
-        console.log("Database Integration Successful! Master Roster:", pokeDex);
+        console.log("Database Integration Successful!", pokeDex);
         
         // TEMPORARY SEED FOR TEAM BATTLE UI TESTING:
         if (pokeDex.length >= 2) {
-            playerTeam = [pokeDex[0]]; // Pikachu
-            enemyTeam = [pokeDex[1]];  // Bulbasaur
+            playerTeam = [pokeDex[0]]; 
+            enemyTeam = [pokeDex[1]];  
+            console.log("player team test", playerTeam);
+            console.log("enemy team test", enemyTeam);
         }
 
     } catch (error) {
