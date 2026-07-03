a1f32fe07c5aff23081c1cf872a32abc153c3ebd
Krishna Murti <128858952+KrishnaMukti@users.noreply.github.com>
2026-07-02 23:05:37 -0400

    Update battle_draw.js

--- js/battle_draw.js
+++ js/battle_draw.js
@@ -47,24 +47,24 @@ function drawBattle() {
 function drawBattleBackground() {
     ctx.fillStyle = "lightgreen";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+     if(isBossBattle){
+         ctx.drawImage(
+             cave_battle,
+             0,
+             0,
+             canvas.width,
+             canvas.height
+         );
+     }
+     if(!isBossBattle) {
+         ctx.drawImage(
+             battle_background,
+             0,
+             0,
+             canvas.width,
+             canvas.height);
+     }
 
-    if (isBossBattle) {
-        ctx.drawImage(
-            boss_battle_background,
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        );
-    } else {
-        ctx.drawImage(
-            battle_background,
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        );
-    }
 }
 function drawPokemonInfo() {
     ctx.fillStyle = "black";
